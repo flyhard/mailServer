f560148ea9749dc980c4d9fc6eca0412a8b797c0
Per Abich <per.abich@gmail.com>
2016-02-19 06:48:56 +0000

    Fixed wget

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,5 @@ echo "postfix postfix/main_mailer_type string Internet site" > preseed.txt
 echo "postfix postfix/mailname string mail.test.abich.com" >> preseed.txt
 debconf-set-selections preseed.txt
 apt-get update &&\
- DEBIAN_FRONTEND=noninteractive apt-get install -y postfix procmail ca-certificates vim curl rsyslog &&\
+ DEBIAN_FRONTEND=noninteractive apt-get install -y postfix procmail ca-certificates vim curl rsyslog wget &&\
  rm -rf /var/lib/apt/lists/*
